3511bd58d7ec025af397c73eb93dcc45c5baa144
Oleg Elizarov <oleg.elizarov@motley.fi>
2019-02-25 16:15:12 +0200

    Add compiled build folder

---
+++ build/precache-manifest.c6179bc111e3c5ae906753f8f4f50f43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bea25c9def9def2ad5bf",
+    "url": "/static/js/main.bea25c9d.chunk.js"
+  },
+  {
+    "revision": "a32daef800f4032b9141",
+    "url": "/static/js/2.a32daef8.chunk.js"
+  },
+  {
+    "revision": "bea25c9def9def2ad5bf",
+    "url": "/static/css/main.80829a35.chunk.css"
+  },
+  {
+    "revision": "57d29b2ba97f623dd5aad5fc00727c30",
+    "url": "/index.html"
+  }
+];
